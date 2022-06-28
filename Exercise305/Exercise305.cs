7027be345b6231bd7c892f2dca7495a78742ce3e
HNDRKGDK <84562602+HNDRKGDK@users.noreply.github.com>
2022-06-28 15:13:47 +0200

    spaceship

--- Exercise305/Exercise305.cs
+++ Exercise305/Exercise305.cs
@@ -3,42 +3,46 @@ using Raylib_cs;
 
 namespace Movement
 {
-	class Exercise305 : SceneNode
-	{
-		// private fields
-		private SpaceShip spaceship;
+    class Exercise305 : SceneNode
+    {
+        // private fields
+        private SpaceShip spaceship;
 
-		// constructor + call base constructor
-		public Exercise305(String t) : base(t)
-		{
-			spaceship = new SpaceShip();
-			AddChild(spaceship);
-		}
+        // constructor + call base constructor
+        public Exercise305(String t) : base(t)
+        {
+            spaceship = new SpaceShip();
+            AddChild(spaceship);
+        }
 
-		// Update is called every frame
-		public override void Update(float deltaTime)
-		{
-			base.Update(deltaTime);
+        // Update is called every frame
+        public override void Update(float deltaTime)
+        {
+            base.Update(deltaTime);
 
-			HandleInput(deltaTime);
-		}
+            HandleInput(deltaTime);
+        }
 
-		private void HandleInput(float deltaTime)
-		{
-			if (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT)) {
-				spaceship.RotateLeft(deltaTime);
-			}
-			if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT)) {
-				spaceship.RotateRight(deltaTime);
-			}
-			if (Raylib.IsKeyDown(KeyboardKey.KEY_UP)) {
-				spaceship.Thrust();
-			}
-			if (Raylib.IsKeyReleased(KeyboardKey.KEY_UP)) {
-				spaceship.NoThrust();
-			}
-		}
+        private void HandleInput(float deltaTime)
+        {
+            if (Raylib.IsKeyDown(KeyboardKey.KEY_A))
+            {
+                spaceship.RotateLeft(deltaTime);
+            }
+            if (Raylib.IsKeyDown(KeyboardKey.KEY_D))
+            {
+                spaceship.RotateRight(deltaTime);
+            }
+            if (Raylib.IsKeyDown(KeyboardKey.KEY_W))
+            {
+                spaceship.Thrust();
+            }
+            if (Raylib.IsKeyReleased(KeyboardKey.KEY_W))
+            {
+                spaceship.NoThrust();
+            }
+        }
 
-	} // class
+    } // class
 
 } // namespace
